0e73903c449aafcff7d83bb333403ada3d50f0b6
mburysek <miroslav.burysek@teskalabs.com>
2026-01-07 12:38:22 +0100

    Create AST printer

---
+++ internal/pkg/golox/ast_printer.go
@@ -0,0 +1,37 @@
+package golox
+
+import "fmt"
+
+type AstPrinter struct{}
+
+func (a *AstPrinter) Print(expr Expr) string {
+	return expr.Accept(a).(string)
+}
+
+func (a *AstPrinter) VisitBinaryExpr(expr *Binary) any {
+	return a.parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right)
+}
+
+func (a *AstPrinter) VisitGroupingExpr(expr *Grouping) any {
+	return a.parenthesize("group", expr.Expression)
+}
+
+func (a *AstPrinter) VisitLiteralExpr(expr *Literal) any {
+	if expr.Value == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("%v", expr.Value)
+}
+
+func (a *AstPrinter) VisitUnaryExpr(expr *Unary) any {
+	return a.parenthesize(expr.Operator.Lexeme, expr.Right)
+}
+
+func (a *AstPrinter) parenthesize(name string, exprs ...Expr) string {
+	result := "(" + name
+	for _, expr := range exprs {
+		result += " " + expr.Accept(a).(string)
+	}
+	result += ")"
+	return result
+}
