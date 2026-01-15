71af47818b9685ff3bfbc2e5f7b0dfb092095ec0
mburysek <miroslav.burysek@teskalabs.com>
2026-01-15 10:55:37 +0100

    Support FOR loops
    - Desugaring FOR loops into WHILE loop

--- internal/pkg/golox/parser.go
+++ internal/pkg/golox/parser.go
@@ -127,16 +127,19 @@ func (p *Parser) varDeclaration() (Stmt, error) {
 	return &Var{Name: nameToken, Initializer: initializer}, nil
 }
 
-// statement -> printStmt | whileStmt | ifStmt | block | expressionStmt ;
+// statement -> printStmt | forStmt | whileStmt | ifStmt | block | expressionStmt ;
 func (p *Parser) statement() (Stmt, error) {
-	if p.match(PRINT) {
-		return p.printStatement()
+	if p.match(IF) {
+		return p.ifStatement()
+	}
+	if p.match(FOR) {
+		return p.forStatement()
 	}
 	if p.match(WHILE) {
 		return p.whileStatement()
 	}
-	if p.match(IF) {
-		return p.ifStatement()
+	if p.match(PRINT) {
+		return p.printStatement()
 	}
 	if p.match(LEFT_BRACE) {
 		statements, err := p.block()
@@ -184,6 +187,80 @@ func (p *Parser) whileStatement() (Stmt, error) {
 	}
 
 	return &While{Condition: condition, Body: body}, nil
+}
+
+// forStmt -> "for" "(" ( varDecl | expressionStmt | ";" ) expression? ";" expression? ")" statement ;
+func (p *Parser) forStatement() (Stmt, error) {
+	_, err := p.consume(LEFT_PAREN, "Expect '(' after 'for'.")
+	if err != nil {
+		return nil, err
+	}
+
+	var initializer Stmt
+	if p.match(SEMICOLON) {
+		initializer = nil
+	} else if p.match(VAR) {
+		initializer, err = p.varDeclaration()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		initializer, err = p.expressionStatement()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var condition Expr
+	if !p.check(SEMICOLON) {
+		condition, err = p.expression()
+		if err != nil {
+			return nil, err
+		}
+	}
+	_, err = p.consume(SEMICOLON, "Expect ';' after loop condition.")
+	if err != nil {
+		return nil, err
+	}
+
+	var increment Expr
+	if !p.check(RIGHT_PAREN) {
+		increment, err = p.expression()
+		if err != nil {
+			return nil, err
+		}
+	}
+	_, err = p.consume(RIGHT_PAREN, "Expect ')' after for clauses.")
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := p.statement()
+	if err != nil {
+		return nil, err
+	}
+
+	// Desugar for loop into while loop
+	if increment != nil {
+		body = &Block{Statements: []Stmt{
+			body,
+			&Expression{Expression: increment},
+		}}
+	}
+
+	if condition == nil {
+		condition = &Literal{Value: true}
+	}
+	body = &While{Condition: condition, Body: body}
+
+	if initializer != nil {
+		body = &Block{Statements: []Stmt{
+			initializer,
+			body,
+		}}
+	}
+
+	return body, nil
 }
 
 // ifStmt -> "if" "(" expression ")" statement ( "else" statement )? ;
