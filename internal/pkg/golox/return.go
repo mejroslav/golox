f312ef627df13c3d6c5c94686d17951be2648677
mburysek <miroslav.burysek@teskalabs.com>
2026-01-16 13:32:32 +0100

    Another comment

--- internal/pkg/golox/return.go
+++ internal/pkg/golox/return.go
@@ -2,6 +2,8 @@ package golox
 
 import "fmt"
 
+// ReturnValue is a special error type used to handle return statements in functions.
+// It carries the return value and allows it to be propagated up the call stack.
 type ReturnValue struct {
 	Keyword *Token
 	Value   any
